bb173e84e7bb13a3d22f6e4678dfad25e23036b3
petererdos <peter.erdos@live.com>
2015-10-13 14:55:54 +0300

    no message

---
+++ Banking/Banking/Banking/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Banking {
+    [TestClass]
+    public class UnitTest1 {
+
+        double suma = 40000;
+        double dobanda = 7.57;
+        double aniperioada = 20;
+        int lunaceruta = 16;
+        
+        [TestMethod]
+        public void PercentsOfRequestedMonth() {
+            Assert.AreEqual(10, CalculateMonthlyPercent());
+        }
+
+        public double CalculateMonthlyPercent() {
+            double months = aniperioada * 12;
+            double[] percents = new double[(int)months];
+            for (int i = 1; i <= (int)months; i++) {
+                double monthlypercent = 100 - (100-(((int)months-(i-1))*100)/100);
+                //percents[i] = monthlypercent;
+                }
+            return (int)months;
+        }
+
+    }
+}
--- Paving/Paving/Paving/UnitTest1.cs
+++ Paving/Paving/Paving/UnitTest1.cs
@@ -4,37 +4,34 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Paving {
     [TestClass]
     public class Paving {
-        double m = 6;
-        double n = 6;
-        double a = 4;
 
         [TestMethod]
         public void CheckTotalNumberOfTilesNeeded() {
-            Assert.AreEqual(4, CalculateTiles());
+            Assert.AreEqual(4, CalculateTiles(6, 6, 4));
         }
 
         [TestMethod]
         public void CheckNumberOfTilesPerWidth() {
-            Assert.AreEqual(2, CalculateMA());
+            Assert.AreEqual(2, CalculateMA(6, 4));
         }
 
         [TestMethod]
         public void CheckNumberOfTilesPerLenght() {
-            Assert.AreEqual(2, CalculateNA());
+            Assert.AreEqual(2, CalculateNA(6, 4));
         }
 
-        public double CalculateMA() {
+        public double CalculateMA(double m, double a) {
             double ma = Math.Ceiling(m / a);
             return ma;
         }
 
-        public double CalculateNA() {
+        public double CalculateNA(double n, double a) {
             double na = Math.Ceiling(n / a);
             return na;
         }
 
-        public double CalculateTiles() {
-            double tiles = CalculateMA() * CalculateNA();
+        public double CalculateTiles(double m, double n, double a) {
+            double tiles = CalculateMA(m, a) * CalculateNA(n, a);
             return tiles;
         }
 
