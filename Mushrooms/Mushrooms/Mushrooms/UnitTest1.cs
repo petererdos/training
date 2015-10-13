bd060b5275e16bd7d2171142910ad788c824f7e8
petererdos <peter.erdos@live.com>
2015-10-13 10:56:09 +0300

    no message

---
+++ Mushrooms/Mushrooms/Mushrooms/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mushrooms {
+    [TestClass]
+    public class Mushrooms {
+        int mushrooms = 12;
+        int x = 2;
+        [TestMethod]
+        public void NumberOfAllMushrooms() {
+            if (mushrooms % x == 0) {
+                Assert.AreEqual(mushrooms, CheckNumberOfAllMushrooms());
+            } else {
+                // Assert.AreEqual(-1, CheckNumberOfMushrooms());
+                Assert.Fail("Total number of mushrooms invalid!");
+            }
+        }
+        [TestMethod]
+        public void CheckNumberOfRedMushhrooms() {
+            Assert.AreEqual(8, NumberOfRedMushrooms());
+        }
+
+        public int NumberOfRedMushrooms() {
+            int white = mushrooms / (x + 1);
+            int red = white * 2;
+            return red;
+        }
+
+        public int CheckNumberOfAllMushrooms() {
+            int white = mushrooms / (x+1);
+            int red = white*2;
+            int result = white + red;
+            return result;
+        }
+    }
+}
