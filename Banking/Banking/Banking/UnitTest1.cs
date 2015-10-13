c14d5bf4f52c6419ca32ec68b092e357b890e6a0
petererdos <peter.erdos@live.com>
2015-10-13 23:27:09 +0300

    no message

--- Banking/Banking/Banking/UnitTest1.cs
+++ Banking/Banking/Banking/UnitTest1.cs
@@ -5,24 +5,26 @@ namespace Banking {
     [TestClass]
     public class UnitTest1 {
 
-        double suma = 40000;
+        /*double suma = 40000;
         double dobanda = 7.57;
         double aniperioada = 20;
-        int lunaceruta = 16;
+        int lunaceruta = 23;*/
         
         [TestMethod]
         public void PercentsOfRequestedMonth() {
-            Assert.AreEqual(10, CalculateMonthlyPercent());
+            Assert.AreEqual(378.77, CalculateMonthlyPercent(40000, 20, 7.57, 39));
         }
 
-        public double CalculateMonthlyPercent() {
-            double months = aniperioada * 12;
-            double[] percents = new double[(int)months];
-            for (int i = 1; i <= (int)months; i++) {
-                double monthlypercent = 100 - (100-(((int)months-(i-1))*100)/100);
-                //percents[i] = monthlypercent;
-                }
-            return (int)months;
+        public double CalculateMonthlyPercent(double suma, int aniperioada, double dobanda, int lunaceruta) {
+            int months = (int)aniperioada * 12;
+            double capital = Math.Round((suma / months), 2, MidpointRounding.AwayFromZero);
+            double dobandalunara = Math.Round((dobanda / 12), 2, MidpointRounding.AwayFromZero);
+            double[] r = new double [months];
+            r[1] = capital + (suma * dobandalunara) / 100;
+            for (int i = 2; i < months; i++) {
+                r[i] = Math.Round((capital + ((suma - (capital * (i - 1))) * dobandalunara) / 100), 2, MidpointRounding.AwayFromZero);
+            }
+            return r[lunaceruta];
         }
 
     }
