a51ff9931482218ebd922eb6de2985b31d55abe8
petererdos <peter.erdos@live.com>
2015-10-13 11:42:26 +0300

    no message

---
+++ Paving/Paving/Paving/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Paving {
+    [TestClass]
+    public class Paving {
+        double m = 6;
+        double n = 6;
+        double a = 4;
+
+        [TestMethod]
+        public void CheckTotalNumberOfTilesNeeded() {
+            Assert.AreEqual(4, CalculateTiles());
+        }
+
+        [TestMethod]
+        public void CheckNumberOfTilesPerWidth() {
+            Assert.AreEqual(2, CalculateMA());
+        }
+
+        [TestMethod]
+        public void CheckNumberOfTilesPerLenght() {
+            Assert.AreEqual(2, CalculateNA());
+        }
+
+        public double CalculateMA() {
+            double ma = Math.Ceiling(m / a);
+            return ma;
+        }
+
+        public double CalculateNA() {
+            double na = Math.Ceiling(n / a);
+            return na;
+        }
+
+        public double CalculateTiles() {
+            double tiles = CalculateMA() * CalculateNA();
+            return tiles;
+        }
+
+    }
+}
