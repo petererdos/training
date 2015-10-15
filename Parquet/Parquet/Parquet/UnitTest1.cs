ab51feda551d2777981ad11f4d81d26b7d2d87c9
petererdos <peter.erdos@live.com>
2015-10-15 10:40:03 +0300

    no message

---
+++ Parquet/Parquet/Parquet/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Parquet {
+    [TestClass]
+    public class Parquet {
+        [TestMethod]
+        public void CheckParquetArea() {
+            Assert.AreEqual(149, CalculateArea(5, 7, 1.380, 0.195),1);
+        }
+
+        public double CalculateArea(double a, double b, double m, double n) {
+            double roomarea = a * b;
+            double parquetarea = m * n;
+            double areadifference = roomarea / parquetarea;
+            double totalparquetarea = areadifference + (areadifference * 0.15);
+            return totalparquetarea;
+        }
+    }
+}
