9ea5a125ab15f377eff8c3341ad06a11679c6e47
petererdos <peter.erdos@live.com>
2015-10-15 09:46:33 +0300

    no message

---
+++ Sports/Sports/Sports/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sports {
+    [TestClass]
+    public class Sports {
+        [TestMethod]
+        public void CheckTheNumberOfPracticeSessions() {
+            Assert.AreEqual(49, CaculatePracticeSessions(7));
+        }
+
+        public int CaculatePracticeSessions(int n) {
+            int totalrounds = 0;
+            for (int i = 1; i <= n; i++) {
+                totalrounds = totalrounds + i;
+            }
+            totalrounds = 2 * totalrounds - n;
+            return totalrounds;
+        }
+    }
+}
