d65f1109be04f2ea444f78af9c50b9182c8b1ada
petererdos <peter.erdos@live.com>
2015-10-16 10:50:03 +0300

    no message

---
+++ Melon/Melon/Melon/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Melon {
+    class Program {
+        static void Main() {
+            Console.Write("Melon kg: ");
+            bool result = false;
+            int kg = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= kg; i++) {
+                if (i % 2 == 0) {
+                    int difference = kg - i;
+                    if (difference % 2 == 0) {
+                        result=true;                        
+                        } 
+                    }                    
+                }
+            if (result == true) {
+                Console.WriteLine("DA");
+            } else {
+                Console.WriteLine("NU");
+            }
+             Console.ReadLine();
+            }
+        }
+    }
