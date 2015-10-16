194adfb8dc83a3a5eac5f089ea1b16be715a48e2
petererdos <peter.erdos@live.com>
2015-10-16 16:50:38 +0300

    no message

---
+++ Farmerland/Farmerland/Farmerland/UnitTest2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Farmerland {
+    [TestClass]
+    public class Farmerland2 {
+        [TestMethod]
+        public void VerifyHeight() {
+            Assert.AreEqual(770, CalculateHeight(230, 770000));
+        }
+
+        [TestMethod]
+        public void VerifyDelta() {
+            Assert.AreEqual(3132900, CalculateDelta(230, 770000));
+        }
+
+        [TestMethod]
+        public void VerifyInitialArea() {
+            Assert.AreEqual(592900, CalculateInitialArea(230, 770000));
+        }
+
+        public int CalculateDelta(int width, int area) {
+            
+            int b = width;
+            int c = -area;
+            int delta = b * b - 4 * c;
+            if (delta>0) return delta;
+            return 0;
+        }
+
+        public double CalculateHeight(int width, int area) {
+            int b = width;
+            int c = -area;
+            double height = ((-b) + Math.Sqrt(CalculateDelta(width, area))) / 2;
+            return Math.Abs(height);
+        }
+
+        public double CalculateInitialArea(int width, int area) {
+            double initialarea = Math.Pow(CalculateHeight(width, area), 2);
+            return initialarea;
+        }
+
+    }
+}
