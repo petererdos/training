e1cc8976c653b26971a5eea197690e3a0517691c
petererdos <peter.erdos@live.com>
2015-10-16 12:23:25 +0300

    no message

---
+++ Melon/Melon/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1 {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void VerifyMelon() {
+            Assert.IsTrue(CheckIfMelonCanBeSplittedInTwoEvenNumberParts(10));
+        }
+
+        public bool CheckIfMelonCanBeSplittedInTwoEvenNumberParts(int kg) {
+            bool result = false;
+            if (kg % 2 == 0) {
+                if ((kg / 2) % 2 == 0) {
+                    result = true;
+                }
+                if (((kg / 2) - 1) % 2 == 0) {
+                    if (((kg / 2) + 1) % 2 == 0) {
+                        result = true;
+                    }
+                }
+            }
+            return result;
+        }
+
+    }
+}
