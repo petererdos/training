ec0e71020026d0bb3add62d8fbc737540b470b3d
petererdos <peter.erdos@live.com>
2015-10-16 15:46:25 +0300

    no message

--- Farmerland/Farmerland/Farmerland/UnitTest1.cs
+++ Farmerland/Farmerland/Farmerland/UnitTest1.cs
@@ -9,6 +9,11 @@ namespace Farmerland {
             Assert.AreEqual(770, CalculateArea(230, 770000));
         }
 
+        [TestMethod]
+        public void ValueFound() {
+            Assert.AreNotEqual(0, CheckIfValueWasFound(230, 770000));
+        }
+
         public int CalculateArea(int b, int area) {
             for (int i = 1; i < area; i++) {
                 int a = i * (i + b);
@@ -16,5 +21,11 @@ namespace Farmerland {
             }
             return 0;
         }
+
+        public int CheckIfValueWasFound(int b, int area) {
+            int valuewasfound = CalculateArea(b, area);
+            return valuewasfound;
+        }
+
     }
 }
