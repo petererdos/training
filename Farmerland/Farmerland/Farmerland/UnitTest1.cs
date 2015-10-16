58c95ed26f985b4ee4d534d008397b7de5532c75
petererdos <peter.erdos@live.com>
2015-10-16 15:04:36 +0300

    no message

---
+++ Farmerland/Farmerland/Farmerland/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Farmerland {
+    [TestClass]
+    public class Farmerland {
+        [TestMethod]
+        public void GuessTheValueOfA() {
+            Assert.AreEqual(770, CalculateArea());
+        }
+
+        public int CalculateArea() {
+            for (int i = 1; i < 770000; i++) {
+                int a = i * (i + 230);
+                if (a == 770000) return i;
+            }
+            return 0;
+        }
+    }
+}
