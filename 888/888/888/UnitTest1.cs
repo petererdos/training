c89ab00ab77713beb5f21ef2f2d13b38100b2838
petererdos <peter.erdos@live.com>
2015-10-19 10:40:14 +0300

    no message

---
+++ 888/888/888/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _888 {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void CheckTrippleEightValidNumber() {
+            Assert.AreEqual(192,TrippleEight(1));
+        }
+
+        [TestMethod]
+        public void CheckTrippleEightInValidNumber() {
+            Assert.AreEqual(0, TrippleEight(1));
+        }
+
+        public double TrippleEight(int k)
+        {
+            int nth=0;
+            double number=1;
+            double value=1;
+            double toreturn=0;
+            if (k > 0) {
+                do {
+                    value = Math.Pow(number, 3);
+                    if (value % 1000 == 888) {
+                        nth++;
+                        toreturn = number;
+                    }
+                    number++;
+                } while (nth != k);
+                return toreturn;
+            }
+            return 0;
+        }
+
+    }
+}
