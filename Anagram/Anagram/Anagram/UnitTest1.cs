b7e1ea2ec38a2c24f705a930c228495b41071a3f
petererdos <peter.erdos@live.com>
2015-10-21 09:43:37 +0300

    Backup

---
+++ Anagram/Anagram/Anagram/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Anagram {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+
+    public int Factorial(int n){
+        int factorial=1;
+        for (int i=2; i<=n; i++) {
+            factorial=factorial*i;
+        }
+        return factorial;
+    }
+
+    public int CalculateAnagrams(string word) {
+
+    }
+}
