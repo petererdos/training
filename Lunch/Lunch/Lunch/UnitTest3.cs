dda3f9d2144365341590408bbf0b7272fe26936e
petererdos <peter.erdos@live.com>
2015-10-21 11:32:18 +0300

    With GCD

---
+++ Lunch/Lunch/Lunch/UnitTest3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lunch {
+    [TestClass]
+    public class UnitTest3 {
+        [TestMethod]
+        public void GCDOfTwoNumbers() {
+            Assert.AreEqual(12, LCM(6, 4));
+        }
+        public int GCD(int a, int b) {
+            int min = Math.Min(a, b);
+            int max = Math.Max(a, b);
+            for (int i = min; i > 0; i--) {
+                if (min % i == 0 && max % i == 0)  return i;
+                }
+            return -1;
+        }
+
+        public int LCM(int a, int b) {
+            if (a>0 && b>0) return (a * b) / GCD(a, b);
+            return -1;
+        }
+
+    }
+}
