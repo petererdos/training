3d5c6d3b98d695fb9b6b4715281abb04922eda7e
petererdos <peter.erdos@live.com>
2015-10-21 10:54:48 +0300

    Simplified (by Alin)

--- Archeology/Archeology/Archeology/UnitTest1.cs
+++ Archeology/Archeology/Archeology/UnitTest1.cs
@@ -25,42 +25,30 @@ namespace Archeology {
         }
 
         public double Pythagoras(double ac, double bc) {
-            double ab = Math.Sqrt(Math.Pow(ac,2) + Math.Pow(bc,2));
-            return ab;
+            return Math.Sqrt(Math.Pow(ac,2) + Math.Pow(bc,2));
         }
 
         public double SemiperimiterOfATriangle(double a, double b, double c) {
-            double p = (a + b + c) / 2;
-            return p;
+            return (a + b + c) / 2;
         }
 
-        public double TriangleAreaWithHeron(double a, double b, double c, double p) {
-            double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
-            return area;
+        public double TriangleAreaWithHeron(double a, double b, double c) {
+            var p = SemiperimiterOfATriangle(a, b, c);
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
         }
 
         public double CalculateArea(double x1, double y1, double x2, double y2, double x3, double y3) {
-            double width1, width2, width3;
-            double lenght1, lenght2, lenght3;
-            double semiper;
-            double hypotenuse1, hypotenuse2, hypotenuse3;
+            var firstSide = CalculateDistance(x1, y1, x2, y2);
+            var secondSide = CalculateDistance(x1, y1, x3, y3);
+            var thirdSide = CalculateDistance(x2, y2, x3, y3);
+                        
+            return TriangleAreaWithHeron(firstSide, secondSide, thirdSide);
+        }
 
-            width1 = DistanceBetweenTwoCoordinates(y1, y2);
-            width2 = DistanceBetweenTwoCoordinates(y1, y3);
-            width3 = DistanceBetweenTwoCoordinates(y2, y3);
-
-            lenght1 = DistanceBetweenTwoCoordinates(x1, x2);
-            lenght2 = DistanceBetweenTwoCoordinates(x1, x3);
-            lenght3 = DistanceBetweenTwoCoordinates(x2, x3);
-
-            hypotenuse1 = Pythagoras(width1, lenght1);
-            hypotenuse2 = Pythagoras(width2, lenght2);
-            hypotenuse3 = Pythagoras(width3, lenght3);
-
-            semiper = SemiperimiterOfATriangle(hypotenuse1, hypotenuse2, hypotenuse3);
-
-            return TriangleAreaWithHeron(hypotenuse1, hypotenuse2, hypotenuse3, semiper);
-            
+        private double CalculateDistance(double x1, double y1, double x2, double y2) {
+            var width = DistanceBetweenTwoCoordinates(y1, y2);
+            var length = DistanceBetweenTwoCoordinates(x1, x2);
+            return Pythagoras(width, length);
         }
 
     }
