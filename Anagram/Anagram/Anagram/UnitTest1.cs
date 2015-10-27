37b1ccd01b5d437dceb94821c87b1bb52772c039
petererdos <peter.erdos@live.com>
2015-10-27 15:13:54 +0200

    Returns the number of anagrams of a given word

--- Anagram/Anagram/Anagram/UnitTest1.cs
+++ Anagram/Anagram/Anagram/UnitTest1.cs
@@ -4,20 +4,47 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Anagram {
     [TestClass]
     public class UnitTest1 {
+
         [TestMethod]
-        public void TestMethod1() {
+        public void AnagramsOfEmptyString() {
+            Assert.AreEqual(-1, CalculateAnagrams(""));
         }
-    }
 
-    public int Factorial(int n){
-        int factorial=1;
-        for (int i=2; i<=n; i++) {
-            factorial=factorial*i;
+
+        [TestMethod]
+        public void AnagramsOfWord_A() {
+            Assert.AreEqual(1, CalculateAnagrams("A"));
         }
-        return factorial;
-    }
 
-    public int CalculateAnagrams(string word) {
+        [TestMethod]
+        public void AnagramsOfWord_IS() {
+            Assert.AreEqual(2, CalculateAnagrams("is"));
+        }
 
+        [TestMethod]
+        public void AnagramsOfWord_ONE() {
+            Assert.AreEqual(6, CalculateAnagrams("one"));
+        }
+
+        [TestMethod]
+        public void AnagramsOfWord_MATH() {
+            Assert.AreEqual(24, CalculateAnagrams("math"));
+        }
+
+
+        public int Factorial(int n) {
+            int factorial = 1;
+            for (int i = 2; i <= n; i++) {
+                factorial = factorial * i;
+            }
+            return factorial;
+        }
+
+        public int CalculateAnagrams(string word) {
+            int letters = word.Length;
+            if (letters < 1) return -1;
+            int anagrams = Factorial(letters);
+            return anagrams;
+        }
     }
 }
