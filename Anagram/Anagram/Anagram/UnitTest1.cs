533defb51c63ccd84280af8d8401158e93113a41
petererdos <peter.erdos@live.com>
2015-10-28 14:40:19 +0200

    Calculates anagrams considering duplicate results

--- Anagram/Anagram/Anagram/UnitTest1.cs
+++ Anagram/Anagram/Anagram/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace Anagram {
     [TestClass]
@@ -40,12 +41,50 @@ namespace Anagram {
             Assert.AreEqual(24, CalculateAnagrams("math"));
         }
 
+        [TestMethod]
+        public void AnagramsOfWord_MMAAAATH() {
+            Assert.AreEqual(840, CalculateAnagrams("mmaaaath"));
+        }
+
+        [TestMethod]
+        public void AnagramsOfWord_AABB() {
+            Assert.AreEqual(6, CalculateAnagrams("aabb"));
+        }
+
         public static bool AreAllLetters(string s) {
             foreach (char c in s) {
                 if (!Char.IsLetter(c))
                     return false;
             }
             return true;
+        }
+
+        public int CountDivisors(List<char> theword, char c) {
+            int count=0;
+            for (int i=0; i<theword.Count;i++){
+                if (theword[i] == c) count++;
+            }
+            return count;
+        }
+
+        public List<char> DeleteProcessedChar(List<char> theword, char c) {
+            int lenght = theword.Count;
+            for (int i = 0; i < lenght; i++) {
+                if (theword[i] == c) {
+                    theword.Remove(c);
+                    i--;
+                    lenght--;
+                }
+            }
+            return theword;
+        }
+
+        public List<char> ConvertToList(string word) {
+            List<char> theword = new List<char>();
+            foreach (char c in word){
+                theword.Add(c);
+            }
+            return theword;
         }
 
         public int Factorial(int n) {
@@ -61,6 +100,17 @@ namespace Anagram {
             if (letters < 1) return -1;
             if (AreAllLetters(word) == false) return -1;
             int anagrams = Factorial(letters);
+            List<char> theword = new List<char>();
+            theword = ConvertToList(word);
+
+
+            do {
+                char c = theword[0];
+                int occurance = CountDivisors(theword, c);
+                anagrams = anagrams / Factorial(occurance);
+                theword = DeleteProcessedChar(theword, c);
+            } while (theword.Count != 0);
+         
             return anagrams;
         }
     }
