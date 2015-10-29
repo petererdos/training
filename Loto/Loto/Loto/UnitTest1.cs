aa0ccf8fdf73968ff6c4ee9e1da98f74eb9a4a3d
Peter Erdos <peter.erdos@live.com>
2015-10-29 18:27:44 +0200

    Calculates the chances of winning at the lottery :)

--- Loto/Loto/Loto/UnitTest1.cs
+++ Loto/Loto/Loto/UnitTest1.cs
@@ -9,6 +9,24 @@ namespace Loto {
             Assert.AreEqual(13983816,Lottery(6,49));
         }
 
+        [TestMethod]
+        public void _5of49()
+        {
+            Assert.AreEqual(1906884, Lottery(5, 49));
+        }
+
+        [TestMethod]
+        public void _4of49()
+        {
+            Assert.AreEqual(211876, Lottery(4, 49));
+        }
+
+        [TestMethod]
+        public void _5of40()
+        {
+            Assert.AreEqual(658008, Lottery(5, 40));
+        }
+
         public ulong Factorial(ulong n) {
             ulong factorial = 1;
             for (ulong i = 2; i <= n; i++) {
@@ -17,8 +35,12 @@ namespace Loto {
             return factorial;
         }
 
-        public ulong Lottery(ulong r, ulong n){
-            return Factorial(n) / (Factorial(n - r) * Factorial(r));
+        public int Lottery(ulong r, ulong n){
+            ulong factorial = 1;
+            for (ulong i = (n - r + 1); i <= n; i++)
+                factorial = factorial * i;
+            factorial = factorial / Factorial(r);
+            return (int)factorial;
         }
 
     }
