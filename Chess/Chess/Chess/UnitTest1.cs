c2ce5f698d41af63864159d4ad290ac8d803e067
Peter Erdos <peter.erdos@live.com>
2015-10-29 18:47:49 +0200

    Calculates the total number of squares of a chess board

---
+++ Chess/Chess/Chess/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chess
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TotalNumberOfSquares()
+        {
+            Assert.AreEqual(204, CalculateChessBoard());
+        }
+
+        public int CalculateChessBoard()
+        {
+            int squares = 0;
+            for (int i=1; i<=8; i++)
+            {
+                squares = squares + (i * i);
+            }
+            return squares;
+        }
+
+    }
+}
