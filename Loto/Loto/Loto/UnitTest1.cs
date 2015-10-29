b9a1b72f9abc72ed90788fb9c4b9ceffa7d0acc8
petererdos <peter.erdos@live.com>
2015-10-29 17:56:24 +0200

    In progress

---
+++ Loto/Loto/Loto/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Loto {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void _6of49() {
+            Assert.AreEqual(13983816,Lottery(6,49));
+        }
+
+        public ulong Factorial(ulong n) {
+            ulong factorial = 1;
+            for (ulong i = 2; i <= n; i++) {
+                factorial = factorial * i;
+            }
+            return factorial;
+        }
+
+        public ulong Lottery(ulong r, ulong n){
+            return Factorial(n) / (Factorial(n - r) * Factorial(r));
+        }
+
+    }
+}
